9cb49cb97a8f54a03c2484cf42bc64390a214e63
IxsCN <Ixs.CN@hotmail.com>
2016-11-21 22:13:10 +0800

    upload project

---
+++ zip.sh
@@ -0,0 +1,7 @@
+#!/bin/sh 
+# Copyright (C) 2016 evenS
+
+workdir="/tmp/data/GeeBlack"
+appname="GeeBlack"
+
+tar -czvf "$workdir"/archive.tgz -C "$workdir"/  manifest.json script bg.png leve-line3.png leve-line3@2x.png overbackgroud.css overwrite.css
